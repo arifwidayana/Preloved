dc04b0695b937bfb7312a06518914526527a974e
Arif Widayana <arifwidayana@gmail.com>
2022-12-08 17:12:00 +0700

    add application and create DI koin

---
+++ app/src/main/java/com/arifwidayana/preloved/Preloved.kt
@@ -0,0 +1,19 @@
+package com.arifwidayana.preloved
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class Preloved: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@Preloved)
+            modules(
+
+            )
+        }
+    }
+}
