b2feeeef103dab0aef7bae87319ec0a071f44184
Arif Widayana <arifwidayana@gmail.com>
2022-12-11 17:43:34 +0700

    create repository shared to all feature

---
+++ shared/src/main/java/com/arifwidayana/shared/data/repository/Repository.kt
@@ -0,0 +1,22 @@
+package com.arifwidayana.shared.data.repository
+
+import com.arifwidayana.core.base.BaseRepository
+import com.arifwidayana.core.base.BaseResponse
+import com.google.gson.Gson
+import com.google.gson.JsonParseException
+import okhttp3.ResponseBody
+import org.koin.java.KoinJavaComponent
+
+open class Repository: BaseRepository() {
+    private val gson: Gson by KoinJavaComponent.inject(Gson::class.java)
+
+    override fun <T> getErrorMessageFromApi(response: T): String {
+        val responseBody = response as ResponseBody
+        return try {
+            val body = gson.fromJson(responseBody.string(), BaseResponse::class.java)
+            body.metadata?.msg ?: "Error Api"
+        } catch (e: JsonParseException) {
+            "Error Api"
+        }
+    }
+}
