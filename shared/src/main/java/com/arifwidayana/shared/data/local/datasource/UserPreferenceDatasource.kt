3c7cd25718db10bf90339b58bbc105ed1bcbe2be
Arif Widayana <arifwidayana@gmail.com>
2022-12-11 17:45:15 +0700

    local data user

---
+++ shared/src/main/java/com/arifwidayana/shared/data/local/datasource/UserPreferenceDatasource.kt
@@ -0,0 +1,36 @@
+package com.arifwidayana.shared.data.local.datasource
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+
+interface UserPreferenceDatasource {
+    suspend fun getUserToken(): Flow<String>
+    suspend fun setUserToken(newUserToken: String)
+    suspend fun deleteUserToken()
+}
+
+class UserPreferenceDatasourceImpl(
+    private val dataStore: DataStore<Preferences>
+): UserPreferenceDatasource {
+    override suspend fun getUserToken(): Flow<String> = flow {
+        dataStore.data.map {
+            it.toPreferences()[UserPreferenceKey.userToken].orEmpty()
+        }
+    }
+
+    override suspend fun setUserToken(newUserToken: String) {
+        dataStore.edit {
+            it[UserPreferenceKey.userToken] = newUserToken
+        }
+    }
+
+    override suspend fun deleteUserToken() {
+        dataStore.edit {
+            it.remove(UserPreferenceKey.userToken)
+        }
+    }
+}
