acae833588bc349e1e618e764e91674e6a674c54
Arif Widayana <arifwidayana@gmail.com>
2022-12-11 17:44:17 +0700

    create datastore file

---
+++ shared/src/main/java/com/arifwidayana/shared/data/local/datasource/UserPreferenceFactory.kt
@@ -0,0 +1,32 @@
+package com.arifwidayana.shared.data.local.datasource
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStoreFile
+import com.arifwidayana.shared.utils.Constant.PREF_USER_TOKEN
+import com.arifwidayana.shared.utils.Constant.USER_PREFERENCE
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+
+class UserPreferenceFactory(
+    private val context: Context
+) {
+    fun create(): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            corruptionHandler = ReplaceFileCorruptionHandler(
+                produceNewData = { emptyPreferences() }),
+            scope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
+            produceFile = { context.preferencesDataStoreFile(USER_PREFERENCE) }
+        )
+    }
+}
+
+object UserPreferenceKey {
+    val userToken = stringPreferencesKey(PREF_USER_TOKEN)
+}
