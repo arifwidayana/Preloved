33ca04205723e88a6d17cf619ce42ef26eb0dbd2
Arif Widayana <arifwidayana@gmail.com>
2022-12-12 14:17:20 +0700

    add object navigation id fragment

---
+++ shared/src/main/java/com/arifwidayana/shared/utils/Navigation.kt
@@ -0,0 +1,10 @@
+package com.arifwidayana.shared.utils
+
+object Navigation {
+    //Id Navigation Fragment
+    const val SPLASH = "splashFragment"
+    const val HOMEPAGE = "homeFragment"
+
+    //Uri Parsing
+    const val HOMEPAGE_PARSE = "Preloved://homeFragment"
+}
