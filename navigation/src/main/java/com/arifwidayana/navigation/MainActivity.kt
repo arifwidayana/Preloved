fd5643718edf1c235d11435283f0e2e2209dde17
Arif Widayana <arifwidayana@gmail.com>
2022-12-12 13:34:35 +0700

    update fragment container and bottom nav

--- navigation/src/main/java/com/arifwidayana/navigation/MainActivity.kt
+++ navigation/src/main/java/com/arifwidayana/navigation/MainActivity.kt
@@ -2,10 +2,35 @@ package com.arifwidayana.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.arifwidayana.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() {
+        binding.apply {
+            val navController = findNavController(R.id.fragmentContainerView)
+            bottomNavigationView.setupWithNavController(navController)
+            navController.addOnDestinationChangedListener{ _, destination, _ ->
+                when(destination.id){
+                    0 -> {
+                        bottomNavigationView.visibility = View.VISIBLE
+                    }
+                    else -> {
+                        bottomNavigationView.visibility = View.GONE
+                    }
+                }
+            }
+        }
     }
 }
