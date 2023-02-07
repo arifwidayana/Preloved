d42ca130119facbf589f4f9d849592e84198271a
Arif Widayana <arifwidayana@gmail.com>
2023-02-07 19:02:15 +0700

    update limit data from db history

--- shared/src/main/java/com/arifwidayana/shared/data/local/service/SearchDao.kt
+++ shared/src/main/java/com/arifwidayana/shared/data/local/service/SearchDao.kt
@@ -15,9 +15,9 @@ interface SearchDao {
     @Query("UPDATE search_history SET updated_at= :updatedAt WHERE searchHistoryName= :searchName")
     suspend fun updateSearchHistory(searchName: String, updatedAt: String)
 
-    @Query("SELECT * FROM search_history")
+    @Query("SELECT * FROM search_history LIMIT 4")
     fun getSearchHistory(): Flow<List<SearchHistoryEntity>>
 
-    @Query("SELECT * FROM search_history WHERE searchHistoryName LIKE :searchName||'%'")
+    @Query("SELECT * FROM search_history WHERE searchHistoryName LIKE :searchName||'%' LIMIT 5")
     fun getFindHistory(searchName: String): Flow<List<SearchHistoryEntity>>
 }
