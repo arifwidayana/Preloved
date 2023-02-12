7d39e5cdc0d1b87914fbb9adaa5e252b25ab1b8e
Arif Widayana <arifwidayana@gmail.com>
2023-02-12 17:06:09 +0700

    add data request and response wishlist

---
+++ shared/src/main/java/com/arifwidayana/shared/data/network/model/response/bid/StatusWishlistProductResponse.kt
@@ -0,0 +1,37 @@
+package com.arifwidayana.shared.data.network.model.response.bid
+
+import com.google.gson.annotations.SerializedName
+
+data class StatusWishlistProductResponse(
+    @SerializedName("name")
+    val name: String?,
+    @SerializedName("message")
+    val message: String?,
+    @SerializedName("product")
+    val product: Product?
+) {
+    data class Product(
+        @SerializedName("id")
+        val id: Int?,
+        @SerializedName("name")
+        val name: String?,
+        @SerializedName("description")
+        val description: String?,
+        @SerializedName("base_price")
+        val basePrice: Int?,
+        @SerializedName("image_url")
+        val imageUrl: String?,
+        @SerializedName("image_name")
+        val imageName: String?,
+        @SerializedName("location")
+        val location: String?,
+        @SerializedName("user_id")
+        val userId: Int?,
+        @SerializedName("status")
+        val status: String?,
+        @SerializedName("createdAt")
+        val createdAt: String?,
+        @SerializedName("updatedAt")
+        val updatedAt: String?
+    )
+}
