adddf7b48ca3f52e2b61b710cc1dacbc7291fcfd
Arif Widayana <arifwidayana@gmail.com>
2023-02-15 20:53:14 +0700

    update constant object field login and register by int

--- shared/src/main/java/com/arifwidayana/shared/utils/Constant.kt
+++ shared/src/main/java/com/arifwidayana/shared/utils/Constant.kt
@@ -24,4 +24,17 @@ object Constant {
     const val DEFAULT_INDEX_PAGE = 1
     const val DEFAULT_INDEX_PER_PAGE = 10
     const val NETWORK_PAGE_SIZE = 25
+
+    // LOGIN
+    const val LOGIN_EMAIL_FIELD = 0
+    const val LOGIN_PASSWORD_FIELD = 1
+
+    // REGISTER
+    const val REGISTER_FIRSTNAME_FIELD = 0
+    const val REGISTER_LASTNAME_FIELD = 1
+    const val REGISTER_PASSWORD_FIELD = 2
+    const val REGISTER_CONFIRM_PASSWORD_FIELD = 3
+    const val REGISTER_EMAIL_FIELD = 4
+    const val REGISTER_PHONE_FIELD = 5
+    const val REGISTER_PRIVACY_FIELD = 6
 }
