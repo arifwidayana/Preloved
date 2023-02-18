d148da353c03cd1e46f9bfb9113edfcf4e8b33a2
Arif Widayana <arifwidayana@gmail.com>
2023-02-18 22:40:46 +0700

    create new base bottom sheet dialog

---
+++ core/src/main/java/com/arifwidayana/core/base/BaseBottomSheetDialogFragment.kt
@@ -0,0 +1,55 @@
+package com.arifwidayana.core.base
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.viewbinding.ViewBinding
+import com.arifwidayana.style.R
+import com.arifwidayana.core.utils.getErrorMessage
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.snackbar.Snackbar
+import java.lang.Exception
+
+abstract class BaseBottomSheetDialogFragment<VB : ViewBinding, VM : ViewModel>(
+    private val bindingFactory: (LayoutInflater) -> VB
+) : BaseContract.BottomSheetDialogFragment, BottomSheetDialogFragment() {
+    private var _binding: VB? = null
+    protected val binding: VB get() = _binding!!
+    protected abstract val viewModel: VM
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bindingFactory.invoke(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        observeData()
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog = BottomSheetDialog(requireContext(), R.style.BottomSheetDialogTheme)
+
+    abstract fun initView()
+    abstract fun observeData()
+
+    override fun showMessageSnackBar(isEnabled: Boolean, message: String?, exception: Exception?) {
+        when {
+            exception != null -> {
+                Snackbar.make(binding.root, requireContext().getErrorMessage(exception), Toast.LENGTH_SHORT).show()
+            }
+            isEnabled -> {
+                Snackbar.make(binding.root, message.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
