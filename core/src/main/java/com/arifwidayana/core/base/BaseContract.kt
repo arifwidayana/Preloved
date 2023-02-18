158b2511f6222ec92f011fab61ae270bf575c294
Arif Widayana <arifwidayana@gmail.com>
2023-02-18 22:39:58 +0700

    add nested interface for different case

--- core/src/main/java/com/arifwidayana/core/base/BaseContract.kt
+++ core/src/main/java/com/arifwidayana/core/base/BaseContract.kt
@@ -5,18 +5,27 @@ import androidx.navigation.NavDirections
 import java.lang.Exception
 
 interface BaseContract {
-    fun moveNavigateUp()
-    fun moveNav(navUp: Int, args: Bundle? = null)
-    fun moveNav(direction: NavDirections)
-    fun moveNav(deepLink: String?)
-    fun moveNav(deepLink: String, idFragmentPopUp: String?, inclusive: Boolean = true)
-    fun showLoading(isVisible: Boolean)
-    fun showContent(isVisible: Boolean)
-    fun showContentEmpty(isVisible: Boolean)
-    fun showMessageToast(isEnabled: Boolean, message: String? = null, exception: Exception? = null)
-    fun showMessageSnackBar(
-        isEnabled: Boolean,
-        message: String? = null,
-        exception: Exception? = null
-    )
+    interface Fragment {
+        fun moveNavigateUp()
+        fun moveNav(navUp: Int, args: Bundle? = null)
+        fun moveNav(direction: NavDirections)
+        fun moveNav(deepLink: String?)
+        fun moveNav(deepLink: String, idFragmentPopUp: String?, inclusive: Boolean = true)
+        fun showLoading(isVisible: Boolean)
+        fun showContent(isVisible: Boolean)
+        fun showContentEmpty(isVisible: Boolean)
+        fun showMessageToast(isEnabled: Boolean, message: String? = null, exception: Exception? = null)
+        fun showMessageSnackBar(
+            isEnabled: Boolean,
+            message: String? = null,
+            exception: Exception? = null
+        )
+    }
+    interface BottomSheetDialogFragment {
+        fun showMessageSnackBar(
+            isEnabled: Boolean,
+            message: String? = null,
+            exception: Exception? = null
+        )
+    }
 }
