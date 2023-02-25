c8b10bfb322e488f4209612d600bdd9ec71c9291
Arif Widayana <arifwidayana@gmail.com>
2023-02-25 15:05:05 +0700

    update data layer bid features

--- feature/bid/src/main/java/com/arifwidayana/bid/data/network/datasource/BidDatasource.kt
+++ feature/bid/src/main/java/com/arifwidayana/bid/data/network/datasource/BidDatasource.kt
@@ -1,9 +1,14 @@
 package com.arifwidayana.bid.data.network.datasource
 
 import com.arifwidayana.bid.data.network.service.BidService
-import com.arifwidayana.shared.data.network.model.request.bid.WishlistProductRequest
-import com.arifwidayana.shared.data.network.model.response.bid.StatusWishlistProductResponse
-import com.arifwidayana.shared.data.network.model.response.bid.WishlistProductResponse
+import com.arifwidayana.core.base.BaseDefaultResponse
+import com.arifwidayana.shared.data.network.model.request.bid.BidProductRequest
+import com.arifwidayana.shared.data.network.model.request.bid.order.OrderProductRequest
+import com.arifwidayana.shared.data.network.model.request.bid.wishlist.WishlistProductRequest
+import com.arifwidayana.shared.data.network.model.response.bid.order.BidProductResponse
+import com.arifwidayana.shared.data.network.model.response.bid.order.OrderProductResponse
+import com.arifwidayana.shared.data.network.model.response.bid.product.StatusWishlistProductResponse
+import com.arifwidayana.shared.data.network.model.response.bid.product.WishlistProductResponse
 import com.arifwidayana.shared.data.network.model.response.home.product.BuyerProductResponse
 
 interface BidDatasource {
@@ -11,6 +16,10 @@ interface BidDatasource {
     suspend fun postWishlistProduct(wishlistProductRequest: WishlistProductRequest): StatusWishlistProductResponse
     suspend fun wishlistProduct(): List<WishlistProductResponse>
     suspend fun deleteWishlistProduct(idWishlist: Int): StatusWishlistProductResponse
+    suspend fun postOrderProduct(bidProductRequest: BidProductRequest): BidProductResponse
+    suspend fun getOrderProduct(): List<OrderProductResponse>
+    suspend fun updateOrderProduct(idOrder: Int, orderProductRequest: OrderProductRequest): OrderProductResponse
+    suspend fun deleteOrderProduct(idOrder: Int): BaseDefaultResponse
 }
 
 class BidDatasourceImpl(
@@ -30,5 +39,27 @@ class BidDatasourceImpl(
 
     override suspend fun deleteWishlistProduct(idWishlist: Int): StatusWishlistProductResponse {
         return bidService.deleteWishlistProduct(idWishlist)
+    }
+
+    override suspend fun postOrderProduct(bidProductRequest: BidProductRequest): BidProductResponse {
+        return bidService.postOrderProduct(bidProductRequest)
+    }
+
+    override suspend fun getOrderProduct(): List<OrderProductResponse> {
+        return bidService.getOrderProduct()
+    }
+
+    override suspend fun updateOrderProduct(
+        idOrder: Int,
+        orderProductRequest: OrderProductRequest
+    ): OrderProductResponse {
+        return bidService.updateOrderProduct(
+            idOrder = idOrder,
+            orderProductRequest = orderProductRequest
+        )
+    }
+
+    override suspend fun deleteOrderProduct(idOrder: Int): BaseDefaultResponse {
+        return bidService.deleteOrderProduct(idOrder)
     }
 }
