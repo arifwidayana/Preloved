946768d42f9f295389808001170119ca0631844f
Arif Widayana <arifwidayana@gmail.com>
2023-03-14 17:47:49 +0700

    wont fix for popbackstack fragment from login

--- feature/account/src/main/java/com/arifwidayana/account/presentation/ui/account/AccountFragment.kt
+++ feature/account/src/main/java/com/arifwidayana/account/presentation/ui/account/AccountFragment.kt
@@ -1,6 +1,6 @@
 package com.arifwidayana.account.presentation.ui.account
 
-import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.*
 import coil.load
 import coil.size.Scale
 import com.arifwidayana.account.R
@@ -9,6 +9,7 @@ import com.arifwidayana.core.base.BaseFragment
 import com.arifwidayana.shared.data.network.model.response.account.UserParamResponse
 import com.arifwidayana.shared.utils.Navigation.HOMEPAGE_URI
 import com.arifwidayana.shared.utils.ext.source
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
 class AccountFragment : BaseFragment<FragmentAccountBinding, AccountViewModel>(
@@ -27,9 +28,6 @@ class AccountFragment : BaseFragment<FragmentAccountBinding, AccountViewModel>(
 
     private fun onClick() {
         binding.apply {
-            materialToolbar.setOnClickListener {
-                moveNav(R.id.action_accountFragment_to_login_nav)
-            }
             tvProfile.setOnClickListener {
                 moveNav(R.id.action_accountFragment_to_profileFragment)
             }
@@ -49,26 +47,44 @@ class AccountFragment : BaseFragment<FragmentAccountBinding, AccountViewModel>(
     }
 
     override fun observeData() {
-        lifecycleScope.apply {
-            launchWhenCreated {
-                viewModel.getUserResult.collect {
-                    it.source(
-                        doOnSuccess = { result ->
-                            setStateView(result.payload)
-                        }
-                    )
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
+                launch {
+                    viewModel.validateTokenUserResult.collect {
+                        it.source(
+                            doOnSuccess = {
+                                noticeDialog(
+                                    positiveNav = R.id.action_accountFragment_to_login_nav,
+                                    negativeNav = HOMEPAGE_URI,
+                                    body = com.arifwidayana.style.R.string.body_login
+                                )
+                            },
+                            doOnError = { error ->
+                                showMessageSnackBar(true, exception = error.exception)
+                            }
+                        )
+                    }
                 }
-            }
-            launchWhenCreated {
-                viewModel.logoutUserResult.collect {
-                    it.source(
-                        doOnSuccess = {
-                            moveNav(deepLink = HOMEPAGE_URI)
-                        },
-                        doOnError = { error ->
-                            showMessageSnackBar(true, exception = error.exception)
-                        }
-                    )
+                launch {
+                    viewModel.getUserResult.collect {
+                        it.source(
+                            doOnSuccess = { result ->
+                                setStateView(result.payload)
+                            }
+                        )
+                    }
+                }
+                launch {
+                    viewModel.logoutUserResult.collect {
+                        it.source(
+                            doOnSuccess = {
+                                moveNav(deepLink = HOMEPAGE_URI)
+                            },
+                            doOnError = { error ->
+                                showMessageSnackBar(true, exception = error.exception)
+                            }
+                        )
+                    }
                 }
             }
         }
--- feature/account/src/main/java/com/arifwidayana/account/presentation/ui/account/AccountViewModel.kt
+++ feature/account/src/main/java/com/arifwidayana/account/presentation/ui/account/AccountViewModel.kt
@@ -6,23 +6,35 @@ import com.arifwidayana.account.domain.UserUseCase
 import com.arifwidayana.core.wrapper.ViewResource
 import com.arifwidayana.shared.data.network.model.response.account.UserParamResponse
 import com.arifwidayana.shared.domain.DeleteUserTokenUseCase
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import com.arifwidayana.shared.domain.ValidateUserTokenUseCase
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class AccountViewModel(
     private val userUseCase: UserUseCase,
-    private val deleteUserTokenUseCase: DeleteUserTokenUseCase
+    private val deleteUserTokenUseCase: DeleteUserTokenUseCase,
+    private val validateUserTokenUseCase: ValidateUserTokenUseCase
 ) : AccountContract, ViewModel() {
     private val _getUserResult = MutableStateFlow<ViewResource<UserParamResponse>>(ViewResource.Empty())
     private val _logoutUserResult = MutableStateFlow<ViewResource<String>>(ViewResource.Empty())
+    private val _validateTokenUserResult = MutableSharedFlow<ViewResource<Boolean>>(replay = 0)
     override val getUserResult: StateFlow<ViewResource<UserParamResponse>> = _getUserResult
     override val logoutUserResult: StateFlow<ViewResource<String>> = _logoutUserResult
+    override val validateTokenUserResult: SharedFlow<ViewResource<Boolean>> = _validateTokenUserResult
 
     override fun getUser() {
         viewModelScope.launch {
-            userUseCase().collect {
-                _getUserResult.value = it
+            validateUserTokenUseCase().collect { status ->
+                when (status.payload) {
+                    true -> {
+                        userUseCase().collect {
+                            _getUserResult.value = it
+                        }
+                    }
+                    else -> {
+                        _validateTokenUserResult.emit(status)
+                    }
+                }
             }
         }
     }
