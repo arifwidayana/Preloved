2a7c1cfeac22ff4562cc5c83add024eb6ba3a894
Arif Widayana <arifwidayana@gmail.com>
2023-04-05 21:07:24 +0700

    change import folder

--- feature/account/src/main/java/com/arifwidayana/account/presentation/ui/profile/ProfileViewModel.kt
+++ feature/account/src/main/java/com/arifwidayana/account/presentation/ui/profile/ProfileViewModel.kt
@@ -2,11 +2,11 @@ package com.arifwidayana.account.presentation.ui.profile
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.arifwidayana.account.domain.UserUseCase
 import com.arifwidayana.account.domain.profile.UpdateProfileUseCase
 import com.arifwidayana.core.wrapper.ViewResource
 import com.arifwidayana.shared.data.network.model.request.account.profile.ProfileUserParamRequest
 import com.arifwidayana.shared.data.network.model.response.account.UserParamResponse
+import com.arifwidayana.shared.domain.UserUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
