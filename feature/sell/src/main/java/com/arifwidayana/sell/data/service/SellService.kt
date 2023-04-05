edb678a70aa9df1d8b14a1105799b11c9775c63e
Arif Widayana <arifwidayana@gmail.com>
2023-04-05 21:09:40 +0700

    add and update data layer sell

--- feature/sell/src/main/java/com/arifwidayana/sell/data/service/SellService.kt
+++ feature/sell/src/main/java/com/arifwidayana/sell/data/service/SellService.kt
@@ -1,27 +1,17 @@
 package com.arifwidayana.sell.data.service
 
 import com.arifwidayana.core.BuildConfig
-import com.arifwidayana.shared.data.network.model.request.sell.SellRequest
+import com.arifwidayana.shared.data.network.model.response.home.category.CategoryResponse
 import com.arifwidayana.shared.data.network.model.response.sell.SellResponse
+import okhttp3.RequestBody
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface SellService {
     @POST(BuildConfig.END_POINT_SELLER_PRODUCT)
-    suspend fun createProduct(@Body sellRequest: SellRequest): SellResponse
-//
-//    @GET(BuildConfig.END_POINT_SELLER_PRODUCT)
-//    suspend fun getProduct(): List<SellResponse>
-//
-//    @GET(BuildConfig.END_POINT_DETAIL_SELLER_PRODUCT)
-//    suspend fun getDetailProduct(): SellResponse
-//
-//    @PUT(BuildConfig.END_POINT_DETAIL_SELLER_PRODUCT)
-//    suspend fun updateProduct()
-//
-//    @PATCH(BuildConfig.END_POINT_DETAIL_SELLER_PRODUCT)
-//    suspend fun statusProduct()
-//
-//    @DELETE(BuildConfig.END_POINT_DETAIL_SELLER_PRODUCT)
-//    suspend fun deleteProduct()
+    suspend fun createProduct(@Body requestBody: RequestBody): SellResponse
+
+    @GET(BuildConfig.END_POINT_SELLER_CATEGORY)
+    suspend fun getCategoryProduct(): List<CategoryResponse>
 }
--- shared/src/main/java/com/arifwidayana/shared/data/network/model/mapper/sell/SellMapper.kt
+++ shared/src/main/java/com/arifwidayana/shared/data/network/model/mapper/sell/SellMapper.kt
@@ -1,29 +1,27 @@
 package com.arifwidayana.shared.data.network.model.mapper.sell
 
 import com.arifwidayana.shared.data.network.model.request.sell.SellParamRequest
-import com.arifwidayana.shared.data.network.model.request.sell.SellRequest
 import com.arifwidayana.shared.utils.mapper.DataObjectMapper
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.asRequestBody
-import java.io.File
 
-object SellMapper : DataObjectMapper<SellRequest, SellParamRequest> {
-    override fun toDataObject(viewParam: SellParamRequest?): SellRequest =
-        SellRequest(
-            name = viewParam?.name,
-            description = viewParam?.description,
-            basePrice = viewParam?.basePrice,
-            categoryId = viewParam?.categoryId,
-            location = viewParam?.location,
-            image = viewParam?.image?.let { requestPartBody(it) }
-        )
+object SellMapper : DataObjectMapper<RequestBody, SellParamRequest> {
+    override fun toDataObject(viewParam: SellParamRequest?): RequestBody =
+        requestPartBody(viewParam)
 
-    private fun requestPartBody(file: File): RequestBody {
-        val requestBody = file.asRequestBody("image/*".toMediaType())
-        return MultipartBody.Builder()
-            .addFormDataPart("image", file.name, requestBody)
-            .build()
+    private fun requestPartBody(viewParam: SellParamRequest?): RequestBody {
+        val requestBuilder = MultipartBody.Builder()
+        viewParam?.let { it ->
+            requestBuilder.setType(MultipartBody.FORM)
+                .addFormDataPart("name", it.name)
+                .addFormDataPart("description", it.description)
+                .addFormDataPart("base_price", it.basePrice.toString())
+                .addFormDataPart("category_ids", it.categoryId.map { map -> map.id }.toString())
+                .addFormDataPart("location", it.location)
+                .addFormDataPart("image", it.image.name, it.image.asRequestBody("image/jpg".toMediaType()))
+        }
+        return requestBuilder.build()
     }
 }
