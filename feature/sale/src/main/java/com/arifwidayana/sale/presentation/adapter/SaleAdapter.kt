1054be3b68240447a4e50da5995b6449aa8f8df6
Arif Widayana <arifwidayana@gmail.com>
2023-04-19 23:26:04 +0700

    tab feature sale

---
+++ feature/sale/src/main/java/com/arifwidayana/sale/presentation/adapter/SaleAdapter.kt
@@ -0,0 +1,30 @@
+package com.arifwidayana.sale.presentation.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.arifwidayana.sale.presentation.ui.history.HistoryFragment
+import com.arifwidayana.sale.presentation.ui.interest.InterestFragment
+import com.arifwidayana.sale.presentation.ui.product.ProductFragment
+import com.arifwidayana.sale.presentation.ui.sold.SoldFragment
+
+class SaleAdapter(
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fragmentManager, lifecycle) {
+    private val listFragment = arrayListOf(
+        ProductFragment(),
+        InterestFragment(),
+        SoldFragment(),
+        HistoryFragment()
+    )
+
+    override fun getItemCount(): Int {
+        return listFragment.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return listFragment[position]
+    }
+}
