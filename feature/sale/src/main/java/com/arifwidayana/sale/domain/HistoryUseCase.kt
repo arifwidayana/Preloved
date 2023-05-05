cdf17fe3d2dd1a4216601df4c0ce60e0e91a1857
Arif Widayana <arifwidayana@gmail.com>
2023-05-05 20:58:53 +0700

    feat: add domain layer product history case

--- feature/sale/src/main/java/com/arifwidayana/sale/domain/HistoryUseCase.kt
+++ feature/sale/src/main/java/com/arifwidayana/sale/domain/HistoryUseCase.kt
@@ -4,7 +4,7 @@ import com.arifwidayana.core.base.BaseUseCase
 import com.arifwidayana.core.wrapper.ViewResource
 import com.arifwidayana.sale.data.repository.SaleRepository
 import com.arifwidayana.shared.data.network.model.mapper.sale.HistoryMapper
-import com.arifwidayana.shared.data.network.model.response.sale.history.HistoryParamResponse
+import com.arifwidayana.shared.data.network.model.response.sale.history.SaleHistoryParamResponse
 import com.arifwidayana.shared.utils.ext.suspendSource
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
@@ -13,8 +13,8 @@ import kotlinx.coroutines.flow.flow
 class HistoryUseCase(
     private val saleRepository: SaleRepository,
     coroutineDispatcher: CoroutineDispatcher
-) : BaseUseCase<Nothing, List<HistoryParamResponse>>(coroutineDispatcher) {
-    override suspend fun execute(param: Nothing?): Flow<ViewResource<List<HistoryParamResponse>>> = flow {
+) : BaseUseCase<Nothing, List<SaleHistoryParamResponse>>(coroutineDispatcher) {
+    override suspend fun execute(param: Nothing?): Flow<ViewResource<List<SaleHistoryParamResponse>>> = flow {
         emit(ViewResource.Loading())
         saleRepository.historyTransaction().collect {
             it.suspendSource(
