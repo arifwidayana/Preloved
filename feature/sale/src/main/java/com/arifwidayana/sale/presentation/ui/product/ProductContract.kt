7b30d7429dd60c6713c374277f851b7cf1d6e46e
Arif Widayana <arifwidayana@gmail.com>
2023-05-05 21:00:29 +0700

    feat: update ui layer on feature product sale

--- feature/sale/src/main/java/com/arifwidayana/sale/presentation/ui/product/ProductContract.kt
+++ feature/sale/src/main/java/com/arifwidayana/sale/presentation/ui/product/ProductContract.kt
@@ -1,3 +1,10 @@
 package com.arifwidayana.sale.presentation.ui.product
 
-interface ProductContract
+import com.arifwidayana.core.wrapper.ViewResource
+import com.arifwidayana.shared.data.network.model.response.sale.product.SaleProductParamResponse
+import kotlinx.coroutines.flow.StateFlow
+
+interface ProductContract {
+    val sellerProductResult: StateFlow<ViewResource<List<SaleProductParamResponse>>>
+    fun getSellerProduct()
+}
--- feature/sale/src/main/java/com/arifwidayana/sale/presentation/ui/product/ProductFragment.kt
+++ feature/sale/src/main/java/com/arifwidayana/sale/presentation/ui/product/ProductFragment.kt
@@ -1,7 +1,14 @@
 package com.arifwidayana.sale.presentation.ui.product
 
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.arifwidayana.core.base.BaseFragment
 import com.arifwidayana.sale.databinding.FragmentProductBinding
+import com.arifwidayana.sale.presentation.adapter.ProductAdapter
+import com.arifwidayana.shared.data.network.model.response.sale.product.SaleProductParamResponse
+import com.arifwidayana.shared.utils.ext.source
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
 class ProductFragment : BaseFragment<FragmentProductBinding, ProductViewModel>(
@@ -11,17 +18,38 @@ class ProductFragment : BaseFragment<FragmentProductBinding, ProductViewModel>(
 
     override fun initView() {
         onView()
-        onClick()
     }
 
     private fun onView() {
-    }
-
-    private fun onClick() {
-        binding.apply {
-        }
+        viewModel.getSellerProduct()
     }
 
     override fun observeData() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
+                launch {
+                    viewModel.sellerProductResult.collect {
+                        it.source(
+                            doOnLoading = {},
+                            doOnSuccess = { source ->
+                                setStateAdapter(source.payload)
+                            },
+                            doOnError = { error ->
+                                showMessageSnackBar(true, exception = error.exception)
+                            }
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setStateAdapter(data: List<SaleProductParamResponse>?) {
+        binding.apply {
+            val adapter = ProductAdapter {
+            }
+            adapter.submitList(data)
+            rvSellerProduct.adapter = adapter
+        }
     }
 }
