62092234c07f1f478082776537f867823fceb001
Arif Widayana <arifwidayana@gmail.com>
2023-05-05 20:51:37 +0700

    feat: update data layer feature sale

--- feature/sale/src/main/java/com/arifwidayana/sale/data/repository/SaleRepository.kt
+++ feature/sale/src/main/java/com/arifwidayana/sale/data/repository/SaleRepository.kt
@@ -2,15 +2,21 @@ package com.arifwidayana.sale.data.repository
 
 import com.arifwidayana.core.wrapper.DataResource
 import com.arifwidayana.sale.data.datasource.SaleDatasource
-import com.arifwidayana.shared.data.network.model.response.sale.history.HistoryResponse
+import com.arifwidayana.shared.data.network.model.response.sale.history.SaleHistoryResponse
+import com.arifwidayana.shared.data.network.model.response.sale.order.SaleOrderResponse
+import com.arifwidayana.shared.data.network.model.response.sale.product.SaleProductResponse
 import com.arifwidayana.shared.data.repository.Repository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
-typealias HistoryDataResource = DataResource<List<HistoryResponse>>
+typealias HistoryDataResource = DataResource<List<SaleHistoryResponse>>
+typealias ProductDataResource = DataResource<List<SaleProductResponse>>
+typealias OrderDataResource = DataResource<List<SaleOrderResponse>>
 
 interface SaleRepository {
     suspend fun historyTransaction(): Flow<HistoryDataResource>
+    suspend fun getSellerProduct(): Flow<ProductDataResource>
+    suspend fun getSellerOrder(): Flow<OrderDataResource>
 }
 
 class SaleRepositoryImpl(
@@ -18,5 +24,13 @@ class SaleRepositoryImpl(
 ) : SaleRepository, Repository() {
     override suspend fun historyTransaction(): Flow<HistoryDataResource> = flow {
         emit(safeNetworkCall { saleDatasource.historyTransaction() })
+    }
+
+    override suspend fun getSellerProduct(): Flow<ProductDataResource> = flow {
+        emit(safeNetworkCall { saleDatasource.getSellerProduct() })
+    }
+
+    override suspend fun getSellerOrder(): Flow<OrderDataResource> = flow {
+        emit(safeNetworkCall { saleDatasource.getSellerOrder() })
     }
 }
--- feature/sale/src/main/java/com/arifwidayana/sale/data/service/SaleService.kt
+++ feature/sale/src/main/java/com/arifwidayana/sale/data/service/SaleService.kt
@@ -1,10 +1,20 @@
 package com.arifwidayana.sale.data.service
 
 import com.arifwidayana.core.BuildConfig
-import com.arifwidayana.shared.data.network.model.response.sale.history.HistoryResponse
+import com.arifwidayana.shared.data.network.model.response.sale.history.SaleHistoryResponse
+import com.arifwidayana.shared.data.network.model.response.sale.order.SaleOrderResponse
+import com.arifwidayana.shared.data.network.model.response.sale.product.SaleProductResponse
+import com.arifwidayana.shared.utils.Constant
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface SaleService {
+    @GET(BuildConfig.END_POINT_SELLER_PRODUCT)
+    suspend fun getSellerProduct(): List<SaleProductResponse>
+
+    @GET(BuildConfig.END_POINT_SELLER_ORDER)
+    suspend fun getSellerOrder(@Query("status") status: String = Constant.PENDING): List<SaleOrderResponse>
+
     @GET(BuildConfig.END_POINT_HISTORY)
-    suspend fun historyTransaction(): List<HistoryResponse>
+    suspend fun historyTransaction(): List<SaleHistoryResponse>
 }
