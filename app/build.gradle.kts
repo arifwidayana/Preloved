fe1f4a9af81fd2b7ebb491b7fd51edb81f731d28
Arif Widayana <arifwidayana@gmail.com>
2023-05-10 16:37:59 +0700

    change to get data from env system

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,10 +25,10 @@ android {
 
     signingConfigs {
         create("productionSigningKey") {
-            storeFile = file(SigningHelper.getValue(SigningHelper.RELEASE_STORE_FILE))
-            storePassword = SigningHelper.getValue(SigningHelper.RELEASE_STORE_PASSWORD)
-            keyAlias = SigningHelper.getValue(SigningHelper.RELEASE_KEY_ALIAS)
-            keyPassword = SigningHelper.getValue(SigningHelper.RELEASE_KEY_PASSWORD)
+            storeFile = file(SigningHelper.RELEASE_STORE_FILE)
+            storePassword = System.getenv(SigningHelper.RELEASE_STORE_PASSWORD)
+            keyAlias = System.getenv(SigningHelper.RELEASE_KEY_ALIAS)
+            keyPassword = System.getenv(SigningHelper.RELEASE_KEY_PASSWORD)
         }
     }
 
