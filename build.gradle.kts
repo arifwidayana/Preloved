f808bda0eb92d68e937dd110547a813e3f7d6e06
Arif Widayana <arifwidayana@gmail.com>
2023-05-14 22:09:18 +0700

    feat: hide dependencies classpath firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     dependencies {
-        classpath(Library.classpathToolsBuild)
-        classpath(Library.classpathGoogleAnalytics)
-        classpath(Library.classpathGoogleCrashlytics)
-        classpath(Library.classpathGoogleMonitoringPerformance)
+//        classpath(Library.classpathToolsBuild)
+//        classpath(Library.classpathGoogleAnalytics)
+//        classpath(Library.classpathGoogleCrashlytics)
+//        classpath(Library.classpathGoogleMonitoringPerformance)
     }
 }
 plugins {
