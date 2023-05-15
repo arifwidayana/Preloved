7f5ee315aeb4792e0ddeaa26c15bf330a5b04503
Arif Widayana <arifwidayana@gmail.com>
2023-05-15 11:52:46 +0700

    feat: default signing key

--- buildSrc/src/main/kotlin/SigningHelper.kt
+++ buildSrc/src/main/kotlin/SigningHelper.kt
@@ -4,19 +4,19 @@ import java.util.*
 
 object SigningHelper {
     // KEY PROPERTIES
-//    private const val KEY_PROPERTIES = "key.properties"
+    private const val KEY_PROPERTIES = "key.properties"
 
-//    const val RELEASE_STORE_FILE = "RELEASE_STORE_FILE"
-    const val RELEASE_STORE_FILE = "preloved.jks"
+    const val RELEASE_STORE_FILE = "RELEASE_STORE_FILE"
+//    const val RELEASE_STORE_FILE = "preloved.jks"
     const val RELEASE_STORE_PASSWORD = "RELEASE_STORE_PASSWORD"
     const val RELEASE_KEY_ALIAS = "RELEASE_KEY_ALIAS"
     const val RELEASE_KEY_PASSWORD = "RELEASE_KEY_PASSWORD"
 
-//    private val keyProperties by lazy {
-//        Properties().apply { load(FileInputStream(File(KEY_PROPERTIES))) }
-//    }
-//
-//    fun getValue(key: String): String {
-//        return keyProperties.getProperty(key)
-//    }
+    private val keyProperties by lazy {
+        Properties().apply { load(FileInputStream(File(KEY_PROPERTIES))) }
+    }
+
+    fun getValue(key: String): String {
+        return keyProperties.getProperty(key)
+    }
 }
