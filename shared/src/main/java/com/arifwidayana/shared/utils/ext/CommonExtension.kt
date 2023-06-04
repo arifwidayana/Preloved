6246f147cc0c9b0cda39f4cdaaa53dec31bab2ab
Arif Widayana <arifwidayana@gmail.com>
2023-06-04 17:40:49 +0700

    feat: extension for direct whatsAppMessage buyer

--- shared/src/main/java/com/arifwidayana/shared/utils/ext/CommonExtension.kt
+++ shared/src/main/java/com/arifwidayana/shared/utils/ext/CommonExtension.kt
@@ -1,21 +1,25 @@
 package com.arifwidayana.shared.utils.ext
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Environment
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import java.io.File
+import java.net.URLEncoder
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.util.*
 
-private val defaultCurrency = DecimalFormat.getCurrencyInstance(Locale("id", "ID"))
-private val formatCurrency = DecimalFormatSymbols(Locale("id", "ID"))
-
 fun convertCurrency(value: Int): String {
+    val defaultCurrency = DecimalFormat.getCurrencyInstance(Locale("id", "ID"))
     return defaultCurrency.format(value)
 }
 
 fun parseCurrencyValue(value: String): String {
+    val formatCurrency = DecimalFormatSymbols(Locale("id", "ID"))
     val defaultDecimalFormat = DecimalFormat("#,###")
     val newCurrency = value.replace("[,.]".toRegex(), "").toBigDecimal()
     defaultDecimalFormat.decimalFormatSymbols = formatCurrency.apply { groupingSeparator = '.' }
@@ -43,5 +47,18 @@ fun uriToFile(context: Context, uri: Uri? = null): File? {
             }
             file
         }
+    }
+}
+
+fun whatsAppMessage(context: Context, number: String, message: String) {
+    try {
+        val uri = "https://api.whatsapp.com/send?phone=$number&text=${URLEncoder.encode(message,"utf-8")}".toUri()
+        val intent = Intent().apply {
+            data = uri
+            setPackage("com.whatsapp")
+        }
+        ContextCompat.startActivity(context, intent, null)
+    } catch (e: Exception) {
+        Toast.makeText(context, "Whatsapp not installed", Toast.LENGTH_SHORT).show()
     }
 }
