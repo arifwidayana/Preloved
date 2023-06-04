f88926b83d13a815feee6ede6424e45b120dee51
Arif Widayana <arifwidayana@gmail.com>
2023-06-04 17:47:54 +0700

    feat: call object list icon from constant object

--- feature/sale/src/main/java/com/arifwidayana/sale/presentation/ui/sale/SaleFragment.kt
+++ feature/sale/src/main/java/com/arifwidayana/sale/presentation/ui/sale/SaleFragment.kt
@@ -9,6 +9,7 @@ import com.arifwidayana.core.base.BaseFragment
 import com.arifwidayana.sale.databinding.FragmentSaleBinding
 import com.arifwidayana.sale.presentation.adapter.SaleAdapter
 import com.arifwidayana.shared.data.network.model.response.account.UserParamResponse
+import com.arifwidayana.shared.utils.Constant
 import com.arifwidayana.shared.utils.Navigation
 import com.arifwidayana.shared.utils.ext.source
 import com.google.android.material.tabs.TabLayoutMediator
@@ -62,12 +63,7 @@ class SaleFragment : BaseFragment<FragmentSaleBinding, SaleViewModel>(
         binding.apply {
             val adapter = SaleAdapter(requireActivity().supportFragmentManager, lifecycle)
             val listTab = resources.getStringArray(com.arifwidayana.style.R.array.tab_feature_sale)
-            val listIcon = arrayOf(
-                com.arifwidayana.style.R.drawable.ic_box,
-                com.arifwidayana.style.R.drawable.ic_love,
-                com.arifwidayana.style.R.drawable.ic_dollar,
-                com.arifwidayana.style.R.drawable.ic_history_transaction
-            )
+            val listIcon = Constant.LIST_ICON
             vpContent.adapter = adapter
             TabLayoutMediator(tlFeature, vpContent) { tab, position ->
                 tab.text = listTab[position]
